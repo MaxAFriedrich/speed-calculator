8b533997aaf6332a89eb2e6c2034f76eedb63ce9
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2024-04-07 20:38:41 +0100

    build mvp

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,72 @@
 <script setup lang="ts">
+import {ref} from 'vue';
+
+import Speed from "@/components/icons/Speed.vue";
+import Elivation from "@/components/icons/Elivation.vue";
+import Distance from "@/components/icons/Distance.vue";
+import Time from "@/components/icons/Time.vue";
+
+
+const speed = ref(5);
+const distance = ref(3);
+const elevation = ref(100);
+const hours = ref(0);
+const minutes = ref(0);
+
+function hoursToMinuets(rawHours: number) {
+  hours.value = Math.floor(rawHours);
+  minutes.value = Math.round((rawHours - hours.value) * 60);
+}
+
+function minuetsToHours() {
+  return hours.value + minutes.value / 60;
+}
+
+function calculate(){
+  const speedValue = speed.value;
+  const distanceValue = distance.value;
+  const elevationValue = elevation.value;
+  const timeValue = minuetsToHours();
+
+  const timeInSeconds = timeValue * 3600;
+  const distanceInMeters = distanceValue * 1000;
+  const timeInSecondsWithElevation = timeInSeconds + (elevationValue / 10);
+  const speedInMetersPerSecond = speedValue / 3.6;
+
+  const timeToCompleteWithElevation = distanceInMeters / speedInMetersPerSecond + timeInSecondsWithElevation;
+
+  hoursToMinuets(timeToCompleteWithElevation / 3600);
+}
+
+calculate();
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-    </div>
-  </header>
-
-  <main>
-  </main>
+  <div>
+    <Speed/>
+    <label for="speed">Speed (km/h)</label>
+    <input type="number" id="speed" v-model="speed" />
+  </div>
+  <div>
+    <Distance/>
+    <label for="distance">Distance (km)</label>
+    <input type="number" id="distance" v-model="distance" />
+  </div>
+  <div>
+    <Elivation/>
+    <label for="elevation">Elevation Gain (m)</label>
+    <input type="number" id="elevation" v-model="elevation" />
+  </div>
+  <div>
+    <Time/>
+    <label>Time </label>
+    <label for="hours">hours</label>
+    <input type="number" id="hours" v-model="hours" />
+    <label for="minutes">minutes</label>
+    <input type="number" id="minutes" v-model="minutes"/>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
